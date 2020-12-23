1ee210d0e27374766e96ac30824a571a940a603a
Avenger2627 <imraninfo97@gmail.com>
2020-12-23 09:40:48 +0530

    [Imran] Add: Abiltiy To Analyse Indian Census Via CensusAdapter

---
+++ IndianStateCensus_Analyser/CSVAdapterFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using IndianStateCensus_Analyser.DTO;
+using IndianStateCensus_Analyser.POCO;
+using IndianStateCensus_Analyser;
+
+namespace IndianStateCensus_Analyser
+{
+    public class CSVAdapterFactory
+    {
+        public Dictionary<string, CensusDTO> LoadCsvData(CensusAnalyser.Country country, string csvFilePath, string dataHeaders)
+        {
+            switch (country)
+            {
+                case (CensusAnalyser.Country.INDIA):
+                    return new IndianCensusAdapter().LoadCensusData(csvFilePath, dataHeaders);
+                default:
+                    throw new CensusAnalyserException("No Such Country", CensusAnalyserException.ExceptionType.NO_SUCH_COUNTRY);
+            }
+        }
+    }
+}
