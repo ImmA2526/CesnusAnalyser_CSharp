79dd678f360c6ab74b6658ef0df656f5deeac2bf
Avenger2627 <imraninfo97@gmail.com>
2020-12-23 09:39:05 +0530

    [Imran] Add: Welcome To Census Analyser

---
+++ IndianStateCensus_Analyser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IndianStateCensus_Analyser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("******Welcome To Census Analyser************");
+        }
+    }
+}
