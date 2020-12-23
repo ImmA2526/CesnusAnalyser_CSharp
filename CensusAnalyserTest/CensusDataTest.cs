6803bf9587c97d7a1bb4eceef0cabbcf542ed2a8
Avenger2627 <imraninfo97@gmail.com>
2020-12-23 09:43:47 +0530

    [Imran] Add: Mathc Number OF record from File

---
+++ CensusAnalyserTest/CensusDataTest.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using IndianStateCensus_Analyser;
+using IndianStateCensus_Analyser.POCO;
+using IndianStateCensus_Analyser.DTO;
+using NUnit.Framework;
+using static IndianStateCensus_Analyser.DTO.CensusAnalyser;
+
+namespace CensusAnalyserTest
+{
+    public class CensusDataTest
+    {
+        static string indianStateCensusHeaders = "State,Population,AreaInSqKm,DensityPerSqKm";
+        static string CensusCountPath = @"C:\Users\imran\Desktop\BRDLB_WORK\DOT_NET\IndianStateCensusDemo\CensusAnalyserTest\CSVFILES\ActualCount.csv";
+        static string fileNotFound = @"C:\Users\imran\Desktop\BRDLB_WORK\DOT_NET\IndianStateCensusDemo\CensusAnalyserTest\CSVFILES\ActualCount_Wrong.csv";
+        static string wrongFileType = @"C:\Users\imran\Desktop\BRDLB_WORK\DOT_NET\IndianStateCensusDemo\CensusAnalyserTest\CSVFILES\ActualCount.txt";
+        static string incorrectDelimeter = @"C:\Users\imran\Desktop\BRDLB_WORK\DOT_NET\IndianStateCensusDemo\CensusAnalyserTest\CSVFILES\IncorrectDelimeter.csv";
+        static string headerIncorrect = @"C:\Users\imran\Desktop\BRDLB_WORK\DOT_NET\IndianStateCensusDemo\CensusAnalyserTest\CSVFILES\HeaderIncorect.csv";
+        
+        IndianStateCensus_Analyser.DTO.CensusAnalyser censusAnalyser;
+        Dictionary<string, CensusDTO> totalRecord;
+
+        [SetUp]
+        public void Setup()
+        {
+            censusAnalyser = new IndianStateCensus_Analyser.DTO.CensusAnalyser();
+            totalRecord = new Dictionary<string, CensusDTO>();
+        }
+
+        /// <summary>
+        /// 1.1 Givens the indian census data file when reade then should return census data count.
+        /// </summary>
+        
+        [Test]
+        public void GivenIndianCensusDataFile_WhenReade_ThenShouldReturnCensusDataCount()
+        {
+            totalRecord = censusAnalyser.LoadCensusData(Country.INDIA, CensusCountPath, indianStateCensusHeaders);
+            Assert.AreEqual(29, totalRecord.Count);
+        }
+    }
+}
